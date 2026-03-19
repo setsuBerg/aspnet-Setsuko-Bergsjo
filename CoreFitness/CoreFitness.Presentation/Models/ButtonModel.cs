3b3b3333523862e13bcc679243c489c4cf9c02d7
Setsu <setea.music@gmail.com>
2026-03-19 12:46:03 +0100

    Hero section layout

---
+++ CoreFitness/CoreFitness.Presentation/Models/ButtonModel.cs
@@ -0,0 +1,8 @@
+﻿namespace CoreFitness.Presentation.Models;
+
+public class ButtonModel
+{
+    public string Text { get; set; }
+    public string Controller { get; set; }
+    public string Action { get; set; }
+}
