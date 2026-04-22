a81ecd819ac23a7e6b1571a72cd381b3283f872f
Setsu <setea.music@gmail.com>
2026-04-22 14:13:46 +0200

    Add unit tests and integration test

---
+++ CoreFitness/Tests/MemberTests.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Aggregates.Members;
+namespace Tests;
+
+public class MemberTests
+{
+    [Fact]
+    public void Create_should_throw_exception_when_userId_is_empty()
+    {
+        //Act & Assert
+        Assert.Throws<ArgumentException>(static () => Member.Create(""));
+    }
+
+    [Fact]
+    public void UpdateInformation_should_update_values_correctly()
+    {
+        //Arrange
+        var member = Member.Create("user1");
+
+        //Act
+        member.UpdateInformation("Hanna", "Suzuki", "0700704444", null);
+
+        //Assert
+        Assert.Equal("Hanna", member.FirstName);
+        Assert.Equal("Suzuki", member.LastName);
+        Assert.Equal("0700704444", member.PhoneNumber);
+    }
+}
---
+++ CoreFitness/Tests/TrainingIntegrationTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Tests;
+
+public class TrainingIntegrationTests : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly HttpClient _client;
+
+    public TrainingIntegrationTests(WebApplicationFactory<Program> factory)
+    {
+        _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task GetTrainings_ReturnsOk()
+    {
+        // Act
+        var response = await _client.GetAsync("/training", TestContext.Current.CancellationToken);
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+
+        var html = await response.Content.ReadAsStringAsync(TestContext.Current.CancellationToken);
+        Assert.Contains("Yoga", html);
+    }
+}
